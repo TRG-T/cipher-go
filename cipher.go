91b413339af122620f8b8357d99e4be5e7e58295
TARGEToff <lockontargt@gmail.com>
2021-11-11 20:53:17 +0100

    added indexOf function and the ability to specify a key greater than 23

--- cipher.go
+++ cipher.go
@@ -2,44 +2,55 @@ package main
 
 import "fmt" 
 
-var latin = [26]byte{
-	'a', 'b', 'c', 'd', 'e', 'f',
-	'g', 'h', 'i', 'j', 'k', 'l',
-	'm', 'n', 'o', 'p', 'q', 'r',
-	's', 't', 'u', 'v', 'w', 'x',
-	'y', 'z',
+var latin = [26]string{
+	"a", "b", "c", "d", "e", "f",
+	"g", "h", "i", "j", "k", "l",
+	"m", "n", "o", "p", "q", "r",
+	"s", "t", "u", "v", "w", "x",
+	"y", "z",
 }
 
-var galactic = [26]rune{
-	'ᔑ',  'ʖ',  'ᓵ',  '↸', 'Ŀ', '⎓',
-	'ㅓ', '〒', '╎',  '፧', 'ꖌ', 'ꖎ',
-	'ᒲ',  'リ', 'フ', '¡', 'ᑑ', '።',
-	'ነ',  'ﬧ',  '⚍', '⍊', '∴', '/',
-	'॥',  'Λ',
+var galactic = [26]string{
+	"ᔑ",  "ʖ",  "ᓵ",  "↸", "Ŀ", "⎓",
+	"ㅓ", "〒", "╎",  "፧", "ꖌ", "ꖎ",
+	"ᒲ",  "リ", "フ", "¡", "ᑑ", "።",
+	"ነ",  "ﬧ",  "⚍", "⍊", "∴", "/",
+	"॥",  "Λ",
 }
 
-func reverse(text []byte) []byte {
-	for i, a:= 0, len(text)-1; i<a; i,a = i+1, a-1 {
+func indexOf(text string) int {
+	a := 0
+	for i:=0; i<len(latin); i++ {
+		if latin[i] == text {
+			a = i
+		}
+	}
+	return a
+}
+
+func reverse(text []string) []string {
+	for i, a:= 0, len(text)-1; i<a; i, a = i+1, a-1 {
 		text[i], text[a] = text[a], text[i]
 	}
 	return text
 }
 
-func cesarEncrypt(text []byte) []byte {
+func ceasarEncrypt(text []string, key int) []string {
 	for i:=0; i<len(text); i++ {
-        if(text[i]+3 > 122) {
-            text[i] -= 22;
+		latinIndex := indexOf(text[i])
+        if latinIndex+key > 26 {
+            text[i] = latin[(latinIndex+key)%26];
         } else {
-            text[i] += 3;
+            text[i] = latin[latinIndex+key];
         }
     }
     return text;
 }
 
 func main() {
-	t := []byte{'a', 'b', 'c'}
-	fmt.Printf("Oryginalny tekst: %v \n", string(t))
+	t := []string{"a", "b", "c", "z"}
+	fmt.Printf("Original text: %v \n", t)
 	reversedText := reverse(t)
-	fmt.Printf("Odwrócony tekst: %v \n", string(reversedText))
-	fmt.Printf("Zaszyfrowany cezarem: %v \n", string(cesarEncrypt(reversedText)))
+	fmt.Printf("Reversed text: %v \n", reversedText)
+	fmt.Printf("Encrypted with Caesar: %v \n", ceasarEncrypt(reversedText, 3))
 }
