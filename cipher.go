155844365129efcd86552f2bc6bddba2e14d80fc
TARGEToff <lockontargt@gmail.com>
2022-01-08 00:47:44 +0100

    added decrypt function

--- cipher.go
+++ cipher.go
@@ -24,11 +24,17 @@ var galactic = [5][6]string{
 var userText string
 var userKey string
 
-func indexOf(letter string) (int, int) {
+func indexOf(letter string, choice int) (int, int) {
 	for i:=0; i<5; i++ {
 		for a:=0; a<6; a++ {
-			if latin[i][a] == letter {
-				return i, a;
+			if choice == 0 {
+				if latin[i][a] == letter {
+					return i, a;
+				}
+			} else {
+				if galactic[i][a] == letter {
+					return i, a;
+				}
 			}
 		}
 	}
@@ -45,7 +51,7 @@ func reverse(text []string) []string {
 func stringToKey(key []string) int {
 	var keyInt int
 	for i:=0; i<len(key); i++ {
-		textRow, textColumn := indexOf(key[i])
+		textRow, textColumn := indexOf(key[i], 0)
 		keyInt += textRow + textColumn
 	}
 	fmt.Println(keyInt)
@@ -54,8 +60,16 @@ func stringToKey(key []string) int {
 
 func encrypt(text []string, key int) string {
 	for i:=0; i<len(text); i++ {
-		latinRow, latinColumn := indexOf(text[i]);
-		text[i] = galactic[latinRow][(latinColumn+key)%6]
+		row, col := indexOf(text[i], 0);
+		text[i] = galactic[row][(col+key)%6]
+    }
+    return strings.Join(text, "");
+}
+
+func decrypt(text []string, key int) string {
+	for i:=0; i<len(text); i++ {
+		row, col := indexOf(text[i], 1);
+		text[i] = latin[row][(col+key)%6]
     }
     return strings.Join(text, "");
 }
@@ -70,18 +84,19 @@ func main() {
 		fmt.Printf("Prompt failed: %v\n", err)
 		return
 	}
-
-	fmt.Printf("You chose %q\n", result)
+	
+	fmt.Println("Enter the text")
+	fmt.Scanln(&userText)
+	fmt.Println("Enter key:")
+	fmt.Scanln(&userKey)
+	key := stringToKey(strings.Split(userKey, ""));
+	text := strings.Split(userText, "");
+	fmt.Printf("Original text: %v \n", text);
+	reversedText := reverse(text);
+	fmt.Printf("Reversed text: %v \n", reversedText);
 	if result == "Encrypt" {
-		fmt.Println("Enter text to encrypt")
-		fmt.Scanln(&userText)
-		fmt.Println("Enter key:")
-		fmt.Scanln(&userKey)
-		key := stringToKey(strings.Split(userKey, ""));
-		text := strings.Split(userText, "");
-		fmt.Printf("Original text: %v \n", text);
-		reversedText := reverse(text);
-		fmt.Printf("Reversed text: %v \n", reversedText);
 		fmt.Printf("Encrypted text: %v \n", encrypt(reversedText, key));
+	} else {
+		fmt.Printf("Decrypted text: %v \n", decrypt(reversedText, key));
 	}
 }
